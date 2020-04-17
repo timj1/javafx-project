d77b1095a6a95899c65ab52cd69a77aabdb75a82
Timo Jurvelin <tintze85@hotmail.com>
2020-04-17 17:21:58 +0300

    init, stop, constructor removed

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -6,29 +6,14 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void init() {
-        System.out.println("init");
-    }
-    @Override
     public void start(Stage stage) {
         stage.setTitle("JavaFX");
         stage.show();
         System.out.println("start");
     }
-    @Override
-    public void stop() {
-        System.out.println("stop");
-    }
 
     public static void main(String args[]) {
-        var app = new Application() {
-            @Override
-            public void start(Stage stage) throws Exception {
 
-            }
-        };
-        System.out.println("constructor");
-
-        app.launch(args);
+        launch(args);
     }
 }
