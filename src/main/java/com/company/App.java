05ddb0b7f870814c8a0fc4b59fb1cb3c80382827
Timo Jurvelin <tintze85@hotmail.com>
2020-04-17 19:27:40 +0300

    group, world button commented / BorderPane, clear button, TextArea added

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,9 +13,16 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        Button world = new Button("World");
-        Group group = new Group(new Button("Hello"), world);
-        Scene scene = new Scene(group, 640, 480);
+
+        BorderPane bPane = new BorderPane();
+        Button clearB = new Button("clear");
+        TextArea textA = new TextArea();
+        bPane.setTop(clearB);
+        bPane.setCenter(textA);
+        
+        //Button world = new Button("World");
+        //Group group = new Group(new Button("Hello"), world);
+        Scene scene = new Scene(bPane, 640, 480);
 
         stage.initStyle(StageStyle.DECORATED);
         //stage.setWidth(640);
@@ -23,8 +32,8 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
 
-        world.setTranslateX(640 - world.getWidth());
-        world.setTranslateY(480 - world.getHeight());
+        //world.setTranslateX(640 - world.getWidth());
+        //world.setTranslateY(480 - world.getHeight());
     }
 
     public static void main(String args[]) {
