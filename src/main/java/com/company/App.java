6abe8e6b0ea1ef2611c1969513ca9cb9b827f062
Timo Jurvelin <tintze85@hotmail.com>
2020-04-17 17:47:38 +0300

    stage size, style, centered

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -2,14 +2,18 @@ package com.company;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setWidth(640);
+        stage.setHeight(480);
+        stage.centerOnScreen();
         stage.setTitle("JavaFX");
         stage.show();
-        System.out.println("start");
     }
 
     public static void main(String args[]) {
