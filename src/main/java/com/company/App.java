c633b885b9783b05163a86fb372b6c0357fe9a4e
Timo Jurvelin <tintze85@hotmail.com>
2020-04-17 17:16:04 +0300

    constructor, init, start, stop

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -4,12 +4,31 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
+    @Override
+    public void init() {
+        System.out.println("init");
+    }
     @Override
     public void start(Stage stage) {
         stage.setTitle("JavaFX");
         stage.show();
+        System.out.println("start");
     }
+    @Override
+    public void stop() {
+        System.out.println("stop");
+    }
+
     public static void main(String args[]) {
-        launch(args);
+        var app = new Application() {
+            @Override
+            public void start(Stage stage) throws Exception {
+
+            }
+        };
+        System.out.println("constructor");
+
+        app.launch(args);
     }
 }
