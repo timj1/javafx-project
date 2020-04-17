707f16d3444667dbca0a876b536082cdca91e18c
Timo Jurvelin <tintze85@hotmail.com>
2020-04-17 15:15:04 +0300

    changed my name to window title.

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX HelloWorld!");
+        stage.setTitle("Timo");
         stage.show();
     }
     public static void main(String args[]) {
