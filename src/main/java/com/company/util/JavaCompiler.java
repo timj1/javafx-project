d80dcf6cbfcab2ffc4513fc093a26dc424b512e0
Timo Jurvelin <tintze85@hotmail.com>
2020-04-27 16:31:38 +0300

    getOutput method added

--- src/main/java/com/company/util/JavaCompiler.java
+++ src/main/java/com/company/util/JavaCompiler.java
@@ -5,52 +5,58 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class JavaCompiler {
 
     public static String compileAndRun(String file) {
         String result = "";
         String fileName = file.substring(file.lastIndexOf("\\") + 1);
-        System.out.println(fileName);
         String folderPath = file.replace(fileName, "");
-        System.out.println(folderPath);
+
         fileName = fileName.replace(".java", "");
         try {
             String[] compile = {"javac", file};
             String[] run = {"java", "-cp", folderPath, fileName};
 
+            // Compile
             var compileProcess = Runtime.getRuntime().exec(compile);
             int fc = compileProcess.getErrorStream().read();
             if( fc != -1 ) {
                 System.out.print((char) fc);
-                //String data = getOutput(compileProcess.getErrorStream());
-                //System.out.println(data);
-                //result += data;
+                String data = getOutput(compileProcess.getErrorStream());
+                System.out.println(data);
+                result += data;
             }
 
             int value = compileProcess.waitFor();
+
+            // Run
             if(value == 0) {
                 var runProcess = Runtime.getRuntime().exec(run);
                 int fcs = compileProcess.getErrorStream().read();
                 if(fcs != -1) {
                     System.out.print((char) fcs);
-                    //print(runProcess.getErrorStream());
+                    String data = getOutput(runProcess.getErrorStream());
+                    System.out.println(data);
+                    result += data;
                 } else {
-                    //print(runProcess.getInputStream());
-                    InputStream inputStream = runProcess.getInputStream();
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                    String line = "";
-                    while ((line = bufferedReader.readLine()) != null) {
-                        System.out.println(line);
-                        result = result + line;
-                        System.out.println(result);
-                    }
-                    bufferedReader.close();
+                    String data = getOutput(runProcess.getInputStream());
+                    System.out.println(data);
+                    result += data;
                 }
             }
         } catch (Exception e) {
             System.out.println(e);
+            result += e;
+        }
+        return result;
+    }
+    public static String getOutput(InputStream stream) throws IOException {
+        String result = "";
+        try(BufferedReader bufferedReader
+                    = new BufferedReader(new InputStreamReader(stream))) {
+            result = bufferedReader.lines().collect(Collectors.joining("\n"));
         }
         return result;
     }
